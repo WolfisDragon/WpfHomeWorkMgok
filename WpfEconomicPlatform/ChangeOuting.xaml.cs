c8d60336e4af269a7634fcbb4d27ea0288315ec1
max28p <maxim131006@gmail.com>
2025-04-17 12:16:14 +0300

    добавил окна изменениекатегории и изменение суммы

---
+++ WpfEconomicPlatform/ChangeOuting.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfEconomicPlatform
+{
+    /// <summary>
+    /// Логика взаимодействия для ChangeCategory.xaml
+    /// </summary>
+    public partial class ChangeOuting : Window
+    {
+        public ChangeOuting()
+        {
+            InitializeComponent();
+        }
+
+        private void exitChangeOuting(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
